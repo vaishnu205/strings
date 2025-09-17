6b1ad1ac63bf12f17130324ed58dff47c7592c02
vaishnu205 <dandadevaishnavi07@gmail.com>
2025-09-17 10:21:16 +0530

    Create count words in string.cpp

---
+++ count words in string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str="vaishnvi";
+    int count=0;
+    for(int i=0;i<str.size();i++){
+        count++;
+
+    }
+    cout<<count;
+}
