754d8357e7df0ca5d144e804aa07af41cd37b53c
vaishnu205 <dandadevaishnavi07@gmail.com>
2025-09-17 10:42:23 +0530

    Create replace substring.cpp

---
+++ replace substring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str1="vaisnavi is smart girls";
+    cout<<str1;
+    cout<<endl;
+    int pos=12;
+    str1.replace(pos,  4  ,"goods");
+    cout<<str1;
+}
